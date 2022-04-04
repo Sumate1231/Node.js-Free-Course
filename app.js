4da8835158045d4e0d5d064e90254c1f6962b88a
Yoyo <102974710+Sumate1231@users.noreply.github.com>
2022-04-04 22:02:39 +0700

    Update code from me

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-const express = require('express');
+const express = require("express");
+const chalk = require("chalk");
+const debug = require("debug")("app");
+const morgan = require("morgan");
+const path = require("path");
+
 const app = express();
-const port = 3000;
-const chalk = require('chalk');
-const debug = require('debug')('app');
-const morgan = require('morgan');
+const PORT = process.env.PORT || 4000;
 
-app.use(morgan('combined'));
+app.use(morgan("combined"));
+app.use(express.static(path.join(__dirname, "/public/")));
 
-app.get("/",(req,res) => {
-    res.send("Hello world New Coding!");
-})
+app.get("/", (req, res) => {
+  res.send("Hello world New Coding!");
+});
 
-
-app.listen(port, () => {
-    debug("Listening on port id",chalk.green(port));
-})
+app.listen(PORT, () => {
+  debug("Listening on port id", chalk.green("  :  ", PORT));
+});
