1b37bd1d6c756e37a7c184c56b4a22047c608050
Yoyo <102974710+Sumate1231@users.noreply.github.com>
2022-04-04 21:13:57 +0700

    Add all file of project

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+
+app.use(morgan('combined'));
+
+app.get("/",(req,res) => {
+    res.send("Hello world");
+})
+
+
+app.listen(port, () => {
+    debug("Listening on port id",chalk.green(port));
+})
